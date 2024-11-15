06ddc1e78c2134b35b4968c158f3db9f2da34509
Evilgenuiscreative <mydesignguy@gmail.com>
2024-11-15 13:00:25 -0500

    Pretty much finished but minor adjustments wanted still

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import './App.css'
+import AudioVisualzation from './components/AudioVisualization/AudioVisualzation'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <main className="flex flex-col items-center sm:items-start">
+          <div className="hero"></div>
+        </main>
+        <section className="main-section">
+          <article className="main-left">
+            <h2 className="text-4xl font-bold">Welcome to my site!</h2>
+            <p className="text-2xl pb-3">Half guy next door, half evil genius mad scientist. </p>
+            <p className="text-xl pb-3">I&apos;m Ian Kleinfeld, a voice talent with a boatload of enthusiasm, energy and creativity. I&apos;m a singer, songwriter, and drummer by hobby, and a voice actor by love. I&apos;ve done professional commercial and theater acting since 1995. </p>
+            <p className="text-xl pb-3">My favorite place to be, my “happy place,”  is in the studio, whether for voice or music. I have a professional studio with Source Connect Standard, planned and tuned by Frank Verderosa, though I always super enjoy going out to a top-tier studio when I have the chance.</p>
+            <p className="text-xl pb-3">I bring professionalism, creativity, energy, problem-solving skills, and FUN to every project I work on.</p>
+            <p className="text-xl pb-3"> I am trained in voice acting through sessions or classes with Anna Garduno, Roger Becker, Andy Roth, Deborah Sperling, and other coaches, seminars, and intentsives.</p> 
+            <p className="text-xl pb-3">I&apos;m trained in stage acting at Cabrillo College, University of California Santa Cruz, and Jean Shelton Acting Studio.</p>
+          </article>
+          <article className="main-right">
+            <h2 className="text-4xl font-bold">Demos</h2>
+            <p className="text-xl">Please have a listen to my demos to see what I can do for you.</p>
+            {/*  Demo player */}
+            <p className='text-xl pb-10'>Commercial Demo</p>
+            <AudioVisualzation audioSrc='commercial'></AudioVisualzation>
+
+            <p className='text-xl pb-10'>Character / Game / Animation Demo</p>
+            <AudioVisualzation audioSrc='character'></AudioVisualzation>
+          </article>
+        </section>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
---
+++ src/components/AudioVisualization/AudioVisualzation.tsx
@@ -0,0 +1,61 @@
+import React, {  useRef, useMemo, useCallback, } from 'react';
+// import { createRoot } from 'react-dom/client'
+import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js'
+import { useWavesurfer } from '@wavesurfer/react'
+import './AudioVisualization.css'
+// import WaveSurfer from 'wavesurfer.js';
+
+// Define the interface for the props
+interface AudioVisualzationProps {
+  audioSrc: string;
+}
+
+const AudioVisualzation: React.FC<AudioVisualzationProps> = ({ audioSrc }) => {
+    let audioSource:string = audioSrc;
+    if (audioSrc === "commercial") {
+        audioSource = "../audio/Ian_Kleinfeld_Commercial_Demo.mp3";
+      } else if (audioSrc ==="character"){
+        audioSource =  "../audio/Ian_Kleinfeld_Character_Game_Animation_Demo.mp3"
+      }
+    
+    const containerRef = useRef(null)
+    const formatTime = (seconds: number) => [seconds / 60, seconds % 60].map((v) => `0${Math.floor(v)}`.slice(-2)).join(':')
+
+    const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+      container: containerRef,
+      height: 100,
+      waveColor: '#ffffff',
+      progressColor: 'rgb(74, 76, 210)',
+      barWidth:2,
+      url: audioSource,
+      plugins: useMemo(() => [Timeline.create()], []),
+
+    })
+  
+ 
+    const onPlayPause = useCallback(() => {
+      wavesurfer && wavesurfer.playPause()
+    }, [wavesurfer])
+
+
+
+
+
+  return (
+  <>
+        <section className='demo'>
+
+        <div ref={containerRef} />
+            <p className='timer'>Current time: {formatTime(currentTime)}</p>
+            <div className="btn-control">
+            <button className={isPlaying ? 'btn-pause' : 'btn'} onClick={onPlayPause} style={{ minWidth: '5em' }}>
+                {isPlaying ? 'Pause' : 'Play'}
+            </button>
+            <a className='btn dl' download href={audioSource} >Download</a> 
+            </div>
+        </section>
+  </>
+  )
+};
+
+export default AudioVisualzation;
