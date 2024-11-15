22c25b8013a677e47be2359813e12a5b6d3d2591
Evilgenuiscreative <mydesignguy@gmail.com>
2024-11-15 16:40:48 -0500

    Catching up with original site design and elements combined with new elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,211 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import './App.css'
-import AudioVisualzation from './components/AudioVisualization/AudioVisualzation'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './App.css';
+import AudioVisualzation from './components/AudioVisualization/AudioVisualzation';
 
 function App() {
+  const handleLink = (event: any) => {
+    event.preventDefault(); // Prevent default Link behavior
+    window.open('/your-route', '_blank'); // Open in new tab
+  };
+
   return (
     <Router>
       <div>
-        <main className="flex flex-col items-center sm:items-start">
-          <div className="hero"></div>
+        <div className='top-header'>
+          <section>
+            <img
+              src='./vobi-head.png'
+              alt='Voiceover by Ian - Animation, Commercial, Narration, Medical Narration, eLearning, Explainer, and more!'
+            />
+            <aside>
+              <div className='ian-name'>Ian Kleinfeld</div>
+              <div className='ian-tag'>Voice Actor Services for You</div>
+            </aside>
+          </section>
+        </div>
+        <nav className='main-menu'>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/services'>Services</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <main className='flex flex-col items-center sm:items-start'>
+          <div className='hero'></div>
         </main>
-        <section className="main-section">
-          <article className="main-left">
-            <h2 className="text-4xl font-bold">Welcome to my site!</h2>
-            <p className="text-2xl pb-3">Half guy next door, half evil genius mad scientist. </p>
-            <p className="text-xl pb-3">I&apos;m Ian Kleinfeld, a voice talent with a boatload of enthusiasm, energy and creativity. I&apos;m a singer, songwriter, and drummer by hobby, and a voice actor by love. I&apos;ve done professional commercial and theater acting since 1995. </p>
-            <p className="text-xl pb-3">My favorite place to be, my “happy place,”  is in the studio, whether for voice or music. I have a professional studio with Source Connect Standard, planned and tuned by Frank Verderosa, though I always super enjoy going out to a top-tier studio when I have the chance.</p>
-            <p className="text-xl pb-3">I bring professionalism, creativity, energy, problem-solving skills, and FUN to every project I work on.</p>
-            <p className="text-xl pb-3"> I am trained in voice acting through sessions or classes with Anna Garduno, Roger Becker, Andy Roth, Deborah Sperling, and other coaches, seminars, and intentsives.</p> 
-            <p className="text-xl pb-3">I&apos;m trained in stage acting at Cabrillo College, University of California Santa Cruz, and Jean Shelton Acting Studio.</p>
+        <section className='main-section'>
+          <article className='main-left'>
+            <h2 className='text-4xl font-bold'>
+              50% guy next door, 50% evil genius mad scientist. 100% your choice.
+            </h2>
+            <p className='text-xl pb-3'>
+              I&apos;m Ian Kleinfeld, a voice talent with a boatload of enthusiasm, energy and
+              creativity. I&apos;m a singer, songwriter, and drummer by hobby, and a voice actor by
+              love. I&apos;ve done professional commercial and theater acting since 1995.{' '}
+            </p>
+            <p className='text-xl pb-3'>
+              My favorite place to be, my “happy place,” is in the studio, whether for voice or
+              music. I have a professional studio with Source Connect Standard, planned and tuned by
+              Frank Verderosa, though I always super enjoy going out to a top-tier studio when I
+              have the chance.
+            </p>
+            <p className='text-xl pb-3'>
+              I bring professionalism, creativity, energy, problem-solving skills, and FUN to every
+              project I work on.
+            </p>
+            <p className='text-xl pb-3'>
+              {' '}
+              I am trained in voice acting through sessions or classes with Anna Garduno, Roger
+              Becker, Andy Roth, Deborah Sperling, and other coaches, seminars, and intentsives.
+            </p>
+            <p className='text-xl pb-3'>
+              I&apos;m trained in stage acting at Cabrillo College, University of California Santa
+              Cruz, and Jean Shelton Acting Studio.
+            </p>
           </article>
-          <article className="main-right">
-            <h2 className="text-4xl font-bold">Demos</h2>
-            <p className="text-xl">Please have a listen to my demos to see what I can do for you.</p>
+          <article className='main-right'>
+            <h2 className='text-4xl font-bold'>Demos</h2>
+            <p className='text-xl'>
+              Please have a listen to my demos to see what I can do for you.
+            </p>
             {/*  Demo player */}
-            <p className='text-xl pb-10'>Commercial Demo</p>
+            <p className='text-xl pb-3 pt-7 font-semibold'>Commercial Demo</p>
             <AudioVisualzation audioSrc='commercial'></AudioVisualzation>
 
-            <p className='text-xl pb-10'>Character / Game / Animation Demo</p>
+            <p className='text-xl pb-3 pt-7 font-semibold'>Character / Game / Animation Demo</p>
             <AudioVisualzation audioSrc='character'></AudioVisualzation>
           </article>
         </section>
+        <section className='bottom-section text-4xl p-10 text-center'>
+          Tell me what you want, and I&apos;ll make it happen!
+        </section>
+        <footer>
+          <p className='footer-text'>
+            Broadcast-ready home studio with Source-Connect, Skype, Hangouts, and Phone |{' '}
+            <a href='19193576004'>(919) 357-6004</a>
+          </p>
+          <section className='footer-flex'>
+            <div className='flex pt-4 gap-4 footer-icons'>
+              <div className='icon'>
+                <Link
+                  to='https://www.linkedin.com/in/voiceoverian/'
+                  onClick={handleLink}
+                >
+                  <img
+                    width={36}
+                    height={36}
+                    alt='LinkedIn VoiceByIan'
+                    src='/icon-linkedin.webp'
+                  />
+                </Link>
+              </div>
+              <div className='icon'>
+                <Link
+                  to='https://www.instagram.com/voicebyian/'
+                  onClick={handleLink}
+                >
+                  <img
+                    width={36}
+                    height={36}
+                    alt='Instagram VoiceByIan'
+                    src='/icon-ig.webp'
+                  />
+                </Link>
+              </div>
+              <div className='icon'>
+                <Link
+                  to='https://twitter.com/VoiceByIan'
+                  onClick={handleLink}
+                >
+                  <img
+                    width={36}
+                    height={36}
+                    alt='Twitter VoiceByIan'
+                    src='/icon-twitter.webp'
+                  />
+                </Link>
+              </div>
+              <div className='icon'>
+                <Link
+                  to='https://www.facebook.com/voicebyian'
+                  onClick={handleLink}
+                >
+                  <img
+                    width={36}
+                    height={36}
+                    src='/icon-fb.webp'
+                    alt='Facebook VoiceByIan'
+                  />
+                </Link>
+              </div>
+            </div>
+            {/* right side */}
+            <div className='source-connect'>
+              <Link
+                to='https://source-elements.com/find/iankleinfeld'
+                onClick={handleLink}
+              >
+                <img
+                  width={203}
+                  height={65}
+                  src='/sourceconnectlogo.webp'
+                  alt='Source Connect Standard in Studio'
+                  className='icon'
+                />
+              </Link>
+            </div>
+            <div className='podcast-right'>
+              <article className='podcast'>
+                <div className='podcast-text'>
+                  <p className='pod-text'>Voiceover is hard to get into.</p>
+                  <p className='pod-text'>
+                    Our podcast interviews successful voice actors, asking
+                    <em>
+                      <strong>Voiceover: How Did You DO It??</strong>,
+                    </em>
+                    and how you can too. Listen in. It’s free!
+                  </p>
+                  <p className='pod-text'>
+                    <Link
+                      to='http://voiceoverpodcast.com'
+                      className='vop-link'
+                      onClick={handleLink}
+                    >
+                      http://voiceoverpodcast.com
+                    </Link>
+                  </p>
+                </div>
+                <div>
+                  <Link
+                    to='http://voiceoverpodcast.com'
+                    onClick={handleLink}
+                  >
+                    <img
+                      width={145}
+                      height={145}
+                      alt='VoiceoverPodcast.com --answers to that question and more!'
+                      src='/HowDidYouDOIt.webp'
+                      className='icon'
+                    />
+                  </Link>
+                </div>
+              </article>
+            </div>
+          </section>
+        </footer>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
--- src/components/AudioVisualization/AudioVisualzation.tsx
+++ src/components/AudioVisualization/AudioVisualzation.tsx
@@ -1,8 +1,8 @@
-import React, {  useRef, useMemo, useCallback, } from 'react';
+import React, { useRef, useMemo, useCallback } from 'react';
 // import { createRoot } from 'react-dom/client'
-import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js'
-import { useWavesurfer } from '@wavesurfer/react'
-import './AudioVisualization.css'
+import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js';
+import { useWavesurfer } from '@wavesurfer/react';
+import './AudioVisualization.css';
 // import WaveSurfer from 'wavesurfer.js';
 
 // Define the interface for the props
@@ -11,51 +11,55 @@ interface AudioVisualzationProps {
 }
 
 const AudioVisualzation: React.FC<AudioVisualzationProps> = ({ audioSrc }) => {
-    let audioSource:string = audioSrc;
-    if (audioSrc === "commercial") {
-        audioSource = "../audio/Ian_Kleinfeld_Commercial_Demo.mp3";
-      } else if (audioSrc ==="character"){
-        audioSource =  "../audio/Ian_Kleinfeld_Character_Game_Animation_Demo.mp3"
-      }
-    
-    const containerRef = useRef(null)
-    const formatTime = (seconds: number) => [seconds / 60, seconds % 60].map((v) => `0${Math.floor(v)}`.slice(-2)).join(':')
+  let audioSource: string = audioSrc;
+  if (audioSrc === 'commercial') {
+    audioSource = '../audio/Ian_Kleinfeld_Commercial_Demo_2024-11-15.mp3';
+  } else if (audioSrc === 'character') {
+    audioSource = '../audio/Ian_Kleinfeld_Character_Game_Animation_Demo_2024-11-15.mp3';
+  }
 
-    const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
-      container: containerRef,
-      height: 100,
-      waveColor: '#ffffff',
-      progressColor: 'rgb(74, 76, 210)',
-      barWidth:2,
-      url: audioSource,
-      plugins: useMemo(() => [Timeline.create()], []),
+  const containerRef = useRef(null);
+  const formatTime = (seconds: number) =>
+    [seconds / 60, seconds % 60].map((v) => `0${Math.floor(v)}`.slice(-2)).join(':');
 
-    })
-  
- 
-    const onPlayPause = useCallback(() => {
-      wavesurfer && wavesurfer.playPause()
-    }, [wavesurfer])
+  const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+    container: containerRef,
+    height: 100,
+    waveColor: '#ffffff',
+    progressColor: 'rgb(129, 131, 240)',
+    barWidth: 2,
+    url: audioSource,
+    plugins: useMemo(() => [Timeline.create()], []),
+  });
 
-
-
-
+  const onPlayPause = useCallback(() => {
+    wavesurfer && wavesurfer.playPause();
+  }, [wavesurfer]);
 
   return (
-  <>
-        <section className='demo'>
-
+    <>
+      <section className='demo'>
         <div ref={containerRef} />
-            <p className='timer'>Current time: {formatTime(currentTime)}</p>
-            <div className="btn-control">
-            <button className={isPlaying ? 'btn-pause' : 'btn'} onClick={onPlayPause} style={{ minWidth: '5em' }}>
-                {isPlaying ? 'Pause' : 'Play'}
-            </button>
-            <a className='btn dl' download href={audioSource} >Download</a> 
-            </div>
-        </section>
-  </>
-  )
+        <p className='timer'>Current time: {formatTime(currentTime)}</p>
+        <div className='btn-control'>
+          <button
+            className={isPlaying ? 'btn-pause' : 'btn'}
+            onClick={onPlayPause}
+            style={{ minWidth: '5em' }}
+          >
+            {isPlaying ? 'Pause' : 'Play'}
+          </button>
+          <a
+            className='btn dl'
+            download
+            href={audioSource}
+          >
+            Download
+          </a>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default AudioVisualzation;
